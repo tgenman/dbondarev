98e2fef3f7fa7a39eb4b7a40316c25b01d80b416
tgenman <tgenman@gmail.com>
2017-08-11 12:51:27 +0500

    task 4122

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -12,7 +12,7 @@ public class Calculate {
 
     public static void main (String[] args) {
     
-        System.out.println("Hello world");
+        System.out.println("Hello World");
     }
 
 }
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Testing calculation.
+*
+* @author Dmitry Bondarev (mailto:tgenman@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+
+package ru.job4j;
