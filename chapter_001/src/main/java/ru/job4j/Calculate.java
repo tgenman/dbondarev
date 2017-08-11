294eedbdfee548b0d7e1e3abeb8fe877112e3a24
tgenman <tgenman@gmail.com>
2017-08-11 12:00:58 +0500

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,13 @@
 package ru.job4j;
 
+/**
+* class for Calculate.
+*
+* @author Dmitry Bondarev (mailto:tgenman@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+
 public class Calculate {
 
     public static void main (String[] args) {
