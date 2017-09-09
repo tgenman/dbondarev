27c253da75505af7ef283321005746cd38dd7f8a
tgenman <tgenman@gmail.com>
2017-09-09 20:34:22 +0500

    task [#225]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+
+import java.util.Arrays;
+
+/**
+ * @author Dmitry Bondarev (mailto:tgenman@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ArrayDuplicate {
+
+    public String[] remove(String[] array) {
+        int correctionLength = array.length;
+        for (int out = 0; out < correctionLength; out++) {
+            for (int in = out + 1; in < correctionLength; in++) {
+                if (array[out].equals(array[in])) {
+                    array[in] = null;
+                    array[in] = array[correctionLength - 1];
+                    correctionLength--;
+                    in--;
+                }
+            }
+        }
+        return Arrays.copyOf(array, correctionLength);
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.collection.IsArrayContainingInAnyOrder.arrayContainingInAnyOrder;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Dmitry Bondarev (mailto:tgenman@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class ArrayDuplicateTest {
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
+        ArrayDuplicate example = new ArrayDuplicate();
+        String[] resultArray = example.remove(new String[]{"Привет", "Мир", "Привет", "Супер", "Мир"});
+        String[] expectArray = {"Привет", "Мир", "Супер"};
+        assertThat(resultArray, arrayContainingInAnyOrder(expectArray));
+    }
+}
