1932006134e47d9c72680b32a296686784669dd2
tgenman <tgenman@gmail.com>
2018-04-21 11:18:50 +0500

    task [#999]

---
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,15 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * Created by tgenman on 4/21/18.
+ */
+public class User {
+	/** Name. */
+	private String name;
+	/** Children */
+	private int children;
+	/** date of birthday */
+	private Calendar birthday;
+}
---
+++ chapter_004/src/main/java/ru/job4j/map/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by tgenman on 4/21/18.
+ */
+package ru.job4j.map;
