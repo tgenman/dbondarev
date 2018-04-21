53105273047a96cba040b2db23bc634ee76afcdc
tgenman <tgenman@gmail.com>
2018-04-21 11:39:28 +0500

    task [#1005]

--- chapter_004/src/main/java/ru/job4j/map/User.java
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,8 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by tgenman on 4/21/18.
@@ -8,8 +10,27 @@ import java.util.Calendar;
 public class User {
 	/** Name. */
 	private String name;
-	/** Children */
+	/** Children. */
 	private int children;
-	/** date of birthday */
+	/** date of birthday. */
 	private Calendar birthday;
+
+	/**
+	 * Constructor.
+	 * @param name String
+	 * @param children int
+	 */
+	public User(String name, int children) {
+		this.name = name;
+		this.children = children;
+	}
+
+	public static void main(String[] args) {
+		User one = new User("Ivan", 3);
+		User two = new User("Ivan", 3);
+		Map<User, Object> testInstance = new HashMap<>();
+		testInstance.put(one, 1);
+		testInstance.put(two, 2);
+		System.out.println(testInstance);
+	}
 }
